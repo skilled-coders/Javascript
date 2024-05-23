f8f2e6c1108cf9cf5d1e8a867865a725e4957639
yash_patil_neovasolutions <yash_patil@neovasolutions.in>
2024-05-23 17:36:42 +0530

    loop object code

---
+++ JSPractice/loop-object.js
@@ -0,0 +1,31 @@
+const person = {
+    name: "Jatin",
+    age: 20,
+    sex: "male"
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.sex);
+
+console.log("==================");
+const keys = Object.keys(person);
+console.log(keys);
+
+keys.forEach((key) => {
+    console.log(person[key]);
+});
+
+console.log("==================");
+const values = Object.values(person);
+console.log(values);
+values.forEach((value) => {
+    console.log(value);
+});
+
+console.log("==================");
+const keyValues = Object.entries(person);
+console.log(keyValues);
+keyValues.forEach((keyValue) => {
+    console.log(keyValue[1]);
+});
