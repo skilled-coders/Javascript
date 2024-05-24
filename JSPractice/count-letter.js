92f51b9bb7ec3f4f46751a3009d3d999d84e9687
yash_patil_neovasolutions <yash_patil@neovasolutions.in>
2024-05-24 16:39:10 +0530

    update count letter code

--- JSPractice/count-letter.js
+++ JSPractice/count-letter.js
@@ -1,6 +1,14 @@
-const fName = "YASH ARUN PATIL YY R";
+const fName = "XYASH ARUN PATIL YY R";
 
-const counter = {};
+const counter = {
+    X: 1,
+    Y: 3,
+    A: 3,
+    S: 1,
+    H: 1,
+};
+let max = 3;
+// let maxOccuredLetter = fName[0];
 
 for (let i = 0; i < fName.length; i++) {
     const letter = fName[i];
@@ -8,12 +16,25 @@ for (let i = 0; i < fName.length; i++) {
     if (letter === " ") {
         continue;
     }
-
     if (counter[letter]) {
-        counter[letter] = counter[letter] + 1;
+        const latestCounter = counter[letter] + 1;
+        counter[letter] = latestCounter;
+        if (latestCounter > max) {
+            // maxOccuredLetter = letter;
+            max = latestCounter;
+        }
     } else {
         counter[letter] = 1;
     }
 }
 
 console.log(JSON.stringify(counter, null, 2));
+// console.log("MAX: ", maxOccuredLetter);
+
+const keys = Object.keys(counter); // [X, Y, A, S, H, R, U, ...]
+for (let i = 0; i < keys.length; i++) {
+    if (counter[keys[i]] === max) {
+        console.log("Correct value: ", keys[i]);
+        break;
+    }
+}
