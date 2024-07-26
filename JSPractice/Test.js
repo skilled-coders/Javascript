a00033b76bfc8d57ff8700b073ccc0dc85203fc2
Yash Patil <yashpatil@Yashs-MacBook-Pro.local>
2024-07-26 17:12:42 +0530

    test

---
+++ JSPractice/Test.js
@@ -0,0 +1,35 @@
+// Input
+const fruits = [
+    {
+        name: "apple",
+        quantity: 10,
+    },
+    {
+        name: "banana",
+        quantity: 8,
+    },
+    {
+        name: "grapes",
+        quantity: 12,
+    },
+    {
+        name: "apple",
+        quantity: 5,
+    },
+    {
+        name: "grapes",
+        quantity: 2,
+    },
+];
+
+// Output
+const quantity = {
+    apple: 15,
+    banana: 8,
+    grapes: 14,
+};
+
+const calculateQantity = (array) => {
+    const quantity = {};
+    return quantity;
+};
